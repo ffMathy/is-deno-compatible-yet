f14edfb29b133affc8a94194b7d880185cd9781c
Mathias <mathias.lykkegaard.lorenzen@lego.com>
2024-11-29 09:41:05 +0100

    fix: add basic react app

--- update.ts
+++ update.ts
@@ -1,4 +1,32 @@
-import {$} from 'bun';
+import {$, file} from 'bun';
+import { glob } from 'glob'
+import { writeFile } from 'fs/promises';
 
-// clone latest deno
-await $`git clone https://github.com/denoland/deno.git`
+// get latest deno
+await $`git submodule update --remote --merge deno`
+
+const testJsonConfigFileText = await file('deno/tests/node_compat/config.jsonc').text();
+const jsonConfigFileWithCommentsRemoved = testJsonConfigFileText
+    .split('\n')
+    .filter(line => !line.trim().startsWith('//'))
+    .join('\n');
+const jsonConfigObject = JSON.parse(jsonConfigFileWithCommentsRemoved);
+const categories = Object.keys(jsonConfigObject.tests);
+
+const finalResult = new Array<any>();
+for(const category of categories) {
+    const testsImplemented = jsonConfigObject.tests[category];
+    const allTestFilePaths = await glob(`deno/tests/node_compat/test/${category}/*`);
+    const allTestFileNames = allTestFilePaths.map(path => path.split('/').pop());
+
+    finalResult.push(...allTestFileNames.map(testFileName => {
+        return {
+            name: category + "/" + testFileName,
+            implemented: testsImplemented.includes(testFileName)
+        }
+    }));
+}
+
+// persist to a JSON file in the react app public folder
+const jsonFilePath = `app/public/tests.json`;
+await writeFile(jsonFilePath, JSON.stringify(finalResult, null, 1));
